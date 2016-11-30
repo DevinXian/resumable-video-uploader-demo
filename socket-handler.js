460d1a347e57edc772954ca923a3d8b36eb75a68
DevinXian <devinxian@gmail.com>
2016-11-30 21:16:24 +0800

    to be confirmed

--- socket-handler.js
+++ socket-handler.js
@@ -1,36 +1,33 @@
 const fs = require('fs')
+const iconv = require('iconv-lite')
+
 const FileCache = {}
 
 module.exports = io => {
 	io.on('connection', socket => {
-
-		socket.emit('news', {hello: 'world'})
-
-		socket.on('event2', data => {
-			console.log(data)
-		})
 
 		socket.on('start', data => {
 			var name = data.name, size = data.size
 			FileCache[name] = {
 				size: size,
 				data: '',
-				uploaded: 0
+				downloaded: 0
 			}
-			let place = 0, filePath = './temp/' + name;
+			let place = 0, filePath = './temp' + name;
 			fs.stat(filePath, (err, stat) => {
 				if (err) {
-					return console.error(err)
+					console.log('file ' + filePath + ' not exits, create new file')
 				}
-				if (stat.isFile()) {
+				if (stat && stat.isFile()) {
 					Object.assign(FileCache[name], {downloaded: stat.size})
 					place = stat.size / 524288
 				}
-				fs.open(filePath, 'a', 755, (err, fd) => {
+				fs.open(filePath, 'a', 0o755, (err, fd) => {
 					if (err) {
 						console.error(err)
 					}
 					Object.assign(FileCache[name], {handler: fd})
+					//TODO check resume function
 					socket.emit('more', {place: place, percent: 0})
 				})
 			})
@@ -45,16 +42,21 @@ module.exports = io => {
 
 			//fully uploaded
 			if (file.downloaded === file.size) {
-				fs.write(file.handler, file.data, null, 'Binary', (err, res) => {
+				console.log('full upload')
+				fs.write(file.handler, file.data, null, 'binary', (err, res) => {
 					if (err) {
 						console.error(err)
 					} else {
-						console.log(res)
+						socket.emit('done', {
+							place: file.downloaded / _KB(512),
+							percent: file.downloaded / file.size * 100
+						})
 					}
 				})
-				//bigger than 10mb, write
 			} else if (file.data.length > _MB(10)) {
-				fs.writeFile(file.handler, file.data, null, 'Binary', (err, res) => {
+				// bigger than 10mb, write
+				console.log('bigger than 10MB')
+				fs.write(file.handler, file.data, null, 'binary', (err, res) => {
 					if (err) return console.error(err)
 
 					//set file cache empty
@@ -65,6 +67,7 @@ module.exports = io => {
 					})
 				})
 			} else {
+				console.log('more data')
 				socket.emit('more', {
 					place: file.downloaded / _KB(512),
 					percent: file.downloaded / file.size * 100
